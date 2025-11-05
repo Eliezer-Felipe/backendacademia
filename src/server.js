68fbdae0a62640e8f80c6df83b9249975920d50b
Eliezer Felipe <eliezerflsilva@gmail.com>
2025-11-05 19:01:38 -0300

    first commit

--- src/server.js
+++ src/server.js
@@ -18,7 +18,12 @@ import professorRoutes from "./routes/professorRoutes.js";
 import personalRoutes from "./routes/personalRoutes.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://frontendacademia.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(express.static('frontend'));
 
